0933146f7ba34e8b80d7cb07cee7db086916e109
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2025-03-09 19:49:54 -0300

    created the password validator

---
+++ src/Backend/EasyFinance.Application/SharedValidators/PasswordValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+
+namespace EasyFinance.Application.SharedValidators;
+public class PasswordValidator<T> : PropertyValidator<T, string>
+{
+    public override bool IsValid(ValidationContext<T> context, string password)
+    {
+        if (string.IsNullOrWhiteSpace(password))
+        {
+            context.MessageFormatter.AppendArgument("ErrorMessage", "Password empty");
+            return false;
+        }
+
+        if (password.Length < 8)
+        {
+            context.MessageFormatter.AppendArgument("ErrorMessage", "Minimum password length is 8 characters");
+            return false;
+        }
+
+        return true;
+    }
+
+    public override string Name => throw new NotImplementedException();
+}
+
--- src/Backend/EasyFinance.Application/UseCases/User/Register/RegisterUserValidator.cs
+++ src/Backend/EasyFinance.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -1,4 +1,5 @@
-﻿using EasyFinance.Communication.Request;
+﻿using EasyFinance.Application.SharedValidators;
+using EasyFinance.Communication.Request;
 using FluentValidation;
 
 namespace EasyFinance.Application.UseCases.User.Register;
@@ -8,6 +9,7 @@ public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
     {
         RuleFor((user) => user.Name).NotEmpty().WithMessage("The name must be filled");
         RuleFor((user) => user.Email).NotEmpty().WithMessage("The email field must be filled");
+        RuleFor((user) => user.Password).SetValidator(new PasswordValidator<RequestRegisterUserJson>());
         When((user) => user.Email != null, () =>
         {
             RuleFor((user) => user.Email).EmailAddress().WithMessage("The email must be a valid email address");
