7db0627f65055737c4453e9092f663819acb544b
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2025-03-22 13:06:56 -0300

    add swagger authorization

--- src/Backend/EasyFinance.API/Program.cs
+++ src/Backend/EasyFinance.API/Program.cs
@@ -6,8 +6,10 @@ using EasyFinance.Infraestructure.Extensions;
 using EasyFinance.Domain.Security.Tokens;
 using EasyFinance.API.Token;
 using EasyFinance.API.BackgroundServices;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+const string AUTHENTICATION_TYPE = "Bearer";
 
 builder.Services.AddControllers();
 
@@ -19,7 +21,37 @@ builder.Services.AddRouting(options =>
 });
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition(AUTHENTICATION_TYPE, new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme.
+                        Enter 'Bearer' [space] and then your token in the text input below.
+                        Example: 'Bearer 123456789abcefgh'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = AUTHENTICATION_TYPE
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = AUTHENTICATION_TYPE,
+                },
+                Scheme = "oauth2",
+                Name = AUTHENTICATION_TYPE,
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 
 builder.Services.AddInfraestructure(builder.Configuration);
 builder.Services.AddApplication(builder.Configuration);
